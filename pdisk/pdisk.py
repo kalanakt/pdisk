bf32fe947f1f098d665136e233411c64d39a09ec
kalanakt <e19198@eng.pdn.ac.lk>
2023-05-07 03:29:54 +0530

    fix error :--> `1.0.2`
    fix : function name : file upload server
    add test setup

--- pdisk/pdisk.py
+++ pdisk/pdisk.py
@@ -1,4 +1,4 @@
-#Copyright (c) 2023. kalanakt
+# Copyright (c) 2023. kalanakt
 
 import re
 import asyncio
@@ -10,9 +10,9 @@ from .type import AccountInfo, AccountStat, ClonFile, DeletedFile, File, FileInf
 
 
 class Pdisk:
-
     "Unofficial PDisk.pro API Wrapper By Kalanakt"
-    def __init__(self, api_key:str):
+
+    def __init__(self, api_key: str):
         self.__api_key = api_key
         self.__base_url = 'https://pdisk.pro/api'
 
@@ -25,24 +25,25 @@ class Pdisk:
         url = f"{self.__base_url}/account/info?key={self.__api_key}"
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.get(url))
         if response.status_code != 200:
-            raise Exception(f"Failed to retrieve account info: {response.text}")
+            raise Exception(
+                f"Failed to retrieve account info: {response.text}")
         data = response.json()
         if data["status"] != 200:
             raise Exception(f"Failed to retrieve account info: {data['msg']}")
         result = data["result"]
         return AccountInfo(result["email"], float(result["balance"]), result["storage_used"])
-    
+
     async def account_stats(self) -> List[AccountStat]:
         url = f"{self.__base_url}/account/stats?key={self.__api_key}"
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.get(url))
         if response.status_code != 200:
-            raise Exception(f"Failed to retrieve account stats: {response.text}")
+            raise Exception(
+                f"Failed to retrieve account stats: {response.text}")
         data = response.json()
         if data["status"] != 200:
             raise Exception(f"Failed to retrieve account stats: {data['msg']}")
         result = data["result"]
         return [AccountStat(int(r["downloads"]), int(r["refs"]), float(r["profit_total"])) for r in result]
-    
 
     ###################### UPLOAD ######################
 
@@ -50,14 +51,15 @@ class Pdisk:
         url = f"{self.__base_url}/upload/server?key={self.__api_key}"
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.get(url))
         if response.status_code != 200:
-            raise Exception(f"Failed to retrieve upload server: {response.text}")
+            raise Exception(
+                f"Failed to retrieve upload server: {response.text}")
         data = response.json()
         if data["status"] != 200:
             raise Exception(f"Failed to retrieve upload server: {data['msg']}")
         return data["result"]
 
     async def upload_file(self, file_path: str) -> List[UploadResponse]:
-        server_url = await self.get_upload_server()
+        server_url = await self.upload_server()
         files = {"file_0": open(file_path, "rb")}
         data = {"sess_id": "", "utype": "prem"}
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.post(server_url, files=files, data=data))
@@ -65,7 +67,7 @@ class Pdisk:
             raise Exception(f"Failed to upload file: {response.text}")
         data = response.json()
         return [UploadResponse(file["file_code"], file["file_status"]) for file in data]
-    
+
     async def upload_remote_file(self, url: str, folder_id: int) -> List[UploadResponse]:
         upload_url = f"{self.__base_url}/upload/url?key={self.__api_key}&url={url}&fld_id={folder_id}"
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.get(upload_url))
@@ -73,7 +75,7 @@ class Pdisk:
             raise Exception(f"Failed to upload remote file: {response.text}")
         data = response.json()
         return [UploadResponse(file["file_code"], None) for file in data]
-    
+
     async def check_upload_status(self, file_code: str) -> str:
         isLink = await self.is_pdisk_link(file_code)
         if isLink:
@@ -85,7 +87,6 @@ class Pdisk:
             raise Exception(f"Failed to check upload status: {response.text}")
         data = response.json()
         return data[0]["file_code"]
-            
 
     ###################### FILE MANAGEMENT ######################
 
@@ -103,9 +104,10 @@ class Pdisk:
             raise Exception(f"Failed to retrieve file info: {data['msg']}")
         result = data["result"]
         return [FileInfo(
-            f["filecode"], f["name"], f["status"], int(f["size"]), f["uploaded"], int(f["downloads"])
+            f["filecode"], f["name"], f["status"], int(
+                f["size"]), f["uploaded"], int(f["downloads"])
         ) for f in result]
-    
+
     async def get_file_list(self, page: int = 1, per_page: int = 20, fld_id: int = None, public: int = None, created: str = None, name: str = None) -> List[File]:
         url = f"{self.__base_url}/file/list?key={self.__api_key}&page={page}&per_page={per_page}"
         if fld_id is not None:
@@ -139,7 +141,7 @@ class Pdisk:
             )
             for file_data in data["result"]["files"]
         ]
-    
+
     async def rename_file(self, file_code: str, new_name: str) -> bool:
         isLink = await self.is_pdisk_link(file_code)
         if isLink:
@@ -151,7 +153,7 @@ class Pdisk:
             raise Exception(f"Failed to rename file: {response.text}")
         data = response.json()
         return data["result"]
-    
+
     async def clone_file(self, file_code: str) -> ClonFile:
         isLink = await self.is_pdisk_link(file_code)
         if isLink:
@@ -164,7 +166,7 @@ class Pdisk:
         data = response.json()
         result = data["result"]
         return ClonFile(result["filecode"], result["url"])
-    
+
     async def set_file_folder(self, file_code: str, folder_id: int):
         isLink = await self.is_pdisk_link(file_code)
         if isLink:
@@ -174,7 +176,7 @@ class Pdisk:
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.get(url))
         if response.status_code != 200:
             raise Exception(f"Failed to clone file: {response.text}")
-    
+
     async def deleted_files(self, file_code: str) -> List[DeletedFile]:
         isLink = await self.is_pdisk_link(file_code)
         if isLink:
@@ -186,10 +188,9 @@ class Pdisk:
             raise Exception(f"Failed to clone file: {response.text}")
         data = response.json()
         result = data["result"]
-        return [DeletedFile(f["deleted_ago_sec"], f["deleted"], f["file_code"], f["name"] ) for f in result]
+        return [DeletedFile(f["deleted_ago_sec"], f["deleted"], f["file_code"], f["name"]) for f in result]
 
-    ###################### FOLDER MANAGEMENT ####################### 
-
+    ###################### FOLDER MANAGEMENT #######################
 
     async def get_folder_list(self, folder_id: int = 0) -> FolderList:
         url = f"{self.__base_url}/folder/list?key={self.__api_key}&fld_id={folder_id}"
@@ -200,9 +201,11 @@ class Pdisk:
         if data["status"] != 200:
             raise Exception(f"Failed to retrieve folder list: {data['msg']}")
         result = data["result"]
-        folders = [Folder(fld["fld_id"], fld["name"], fld["code"]) for fld in result["folders"]]
-        files = [File(f["file_id"], f["name"], f["file_code"], f["uploaded"], f["fld_id"]) for f in result["files"]]
-        return FolderList(folders, files) 
+        folders = [Folder(fld["fld_id"], fld["name"], fld["code"])
+                   for fld in result["folders"]]
+        files = [File(f["file_id"], f["name"], f["file_code"],
+                      f["uploaded"], f["fld_id"]) for f in result["files"]]
+        return FolderList(folders, files)
 
     async def create_folder(self, parent_id: int, name: str) -> int:
         url = f"{self.__base_url}/folder/create?key={self.__api_key}&parent_id={parent_id}&name={name}"
@@ -214,7 +217,7 @@ class Pdisk:
             raise Exception(f"Failed to create folder: {data['msg']}")
         result = data["result"]
         return int(result["fld_id"])
-    
+
     async def rename_folder(self, folder_id: int, new_name: str) -> bool:
         url = f"{self.__base_url}/folder/rename?fld_id={folder_id}&name={new_name}&key={self.__api_key}"
         response = await asyncio.get_event_loop().run_in_executor(None, lambda: requests.get(url))
@@ -226,10 +229,10 @@ class Pdisk:
         return data["result"] == "true"
 
     @staticmethod
-    async def is_pdisk_link(link:str) -> bool:
+    async def is_pdisk_link(link: str) -> bool:
         domain = urlparse(link).netloc
         return 'pdisk.pro' in domain
-    
+
     @staticmethod
-    async def extract_file_code(link:str) -> bool:
+    async def extract_file_code(link: str) -> bool:
         return link.replace('https://pdisk.pro/', '')
